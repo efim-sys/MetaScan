06713324deaa7ea59f2d00c956a87245eac43598
efim-sys <81066363+efim-sys@users.noreply.github.com>
2023-09-02 15:07:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -27,11 +27,23 @@ function draw() {
 		
 }
 
-function printCanvas() {
-	let win=window.open();
-	win.document.write("<br><img style='width: 1000; height: 120' src='"+c.toDataURL()+"'/>");
-	win.print();
-	win.location.reload();
+function printCanvas()
+{
+    var dataUrl = c.toDataURL(); //attempt to save base64 string to server using this var
+    var windowContent = '<!DOCTYPE html>';
+    windowContent += '<html>'
+    windowContent += '<head><title>Print canvas</title></head>';
+    windowContent += '<body>'
+    windowContent += '<img style="width: 1000; height: 120" src="' + dataUrl + '">';
+    windowContent += '</body>';
+    windowContent += '</html>';
+    var printWin = window.open('','','width=340,height=260');
+    printWin.document.open();
+    printWin.document.write(windowContent);
+    printWin.document.close();
+    printWin.focus();
+    printWin.print();
+    printWin.close();
 }
 
 barcode.addEventListener("load", function() {
